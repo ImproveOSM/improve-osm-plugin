c3cfc81d1eef4d5d55e52fe52a80663ab313ab88
beata.jancso <beata.jancso@952499c0-9860-4475-ab66-9e54cbe79f4a>
2016-02-02 12:27:12 +0000

    removed sysout
    git-svn-id: svn://svn-srv/improveOsm/plugin/branches/versioning@339 952499c0-9860-4475-ab66-9e54cbe79f4a

--- src/org/openstreetmap/josm/plugins/improveosm/service/BaseService.java
+++ src/org/openstreetmap/josm/plugins/improveosm/service/BaseService.java
@@ -58,7 +58,6 @@ public class BaseService {
      * @throws ServiceException if the operation fail
      */
     public <T> T executeGet(final String url, final Class<T> responseType) throws ServiceException {
-        System.out.println("GET :" + url);
         String response = null;
         try {
             response = new HttpConnector(url, HttpMethod.GET).read();
@@ -79,7 +78,6 @@ public class BaseService {
      */
     public <T> T executePost(final String url, final String content, final Class<T> responseType)
             throws ServiceException {
-        System.out.println("POST: " + url);
         String response = null;
         try {
             final HttpConnector connector = new HttpConnector(url, HttpMethod.POST);
